2d045fc09566d044da2f442d2635b799e7d1a6ea
zw6666 <2714709357@qq.com>
2018-10-27 21:01:41 +0800

    Add files via upload
    Promise主页面

--- in-love/src/containers/Promise/index.jsx
+++ in-love/src/containers/Promise/index.jsx
@@ -2,17 +2,90 @@ import React from 'react'
 //import logo from '../../public/images/logo.svg'
 //import BottomNav from '../../components/BottomNav'
 import './home.css'
+import { NavBar, Icon,List, Switch,TextareaItem } from 'antd-mobile';
+import { createForm } from 'rc-form';
+
+
+
+class SwitchExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false,
+      checked1: true,
+    };
+  }
+
+  render() {
+    const { getFieldProps } = this.props.form;
+    return (
+      <List>
+        <List.Item
+          extra={<Switch
+            {...getFieldProps('Switch1', {
+              initialValue: !this.state.checked1,
+              valuePropName: 'checked',
+              onChange: (val) => {
+                console.log(val);
+                // Do not `setState` with rc-form
+                // this.setState({ checked1: val });
+              },
+            })}
+            onClick={(checked) => {
+              // set new value
+              this.props.form.setFieldsValue({
+                Switch1: checked,
+              });
+            }}
+          />}
+        >Public</List.Item>
+
+      </List>
+    );
+  }
+}
+
+const Se = createForm()(SwitchExample);
+
+
+class TextareaItemExample extends React.Component {
+  render() {
+    const { getFieldProps } = this.props.form;
+    return (
+      <div>
+        <List renderHeader={() => 'Secret'}>
+          <TextareaItem
+            {...getFieldProps('count', {
+    
+            })}
+            rows={8}
+            count={300}
+          />
+        </List>
+      </div>
+    );
+  }
+}
+
+const TextareaItemExampleWrapper = createForm()(TextareaItemExample);
 
 const Home = () => (
   <div>
-    <div className="home__logo--container">
-      <span className="home__logo--text">First Forever </span>
+    <div>
+    <NavBar
+      mode="light"
+      icon={<Icon type="left" />}
+      onLeftClick={() => console.log('onLeftClick')}
+      rightContent={[
+        <Icon key="1" type="ellipsis" />,
+      ]}
+    >In Love</NavBar>
     </div>
-    <div className="home__slogan--container">
-      <h1 className="home__slogan--title">最初即永恒</h1>
-      <p className="home__slogan--text">
-        人生有很多的第一次，如第一次恋爱，第一次找到工作，第一次领到工资等等，但是随着时间的流逝，还来不及回味就已经过去，如果有一个地方能让你存下这一刻的时光，永恒流传，你愿意留下你的时光吗？
-      </p>
+    <div>
+      <TextareaItemExampleWrapper />
+    </div>
+    <div>
+    <Se />
     </div>
   </div>
 )
